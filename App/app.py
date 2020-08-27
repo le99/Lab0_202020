fc7f4de5defeec923fbe49ad136db85f17ac1e2d
lef <le.florez602@uniandes.edu.co>
2020-08-27 09:16:25 -0500

    Lectura de archivos grandes

--- App/app.py
+++ App/app.py
@@ -1,143 +1,90 @@
-"""
- * Copyright 2020, Departamento de sistemas y Computación, Universidad de Los Andes
- * 
- * Contribución de:
- *
- * Cristian Camilo Castellanos
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- """
-
-"""
-  Este módulo es una aplicación básica con un menú de opciones para cargar datos, contar elementos, y hacer búsquedas sobre una lista.
-"""
-
-import config as cf
-import sys
+import config as conf
 import csv
+from ADT import list as lt
+from DataStructures import listiterator
 from time import process_time 
 
-def loadCSVFile (file, lst, sep=";"):
-    """
-    Carga un archivo csv a una lista
-    Args:
-        file 
-            Archivo de texto del cual se cargaran los datos requeridos.
-        lst :: []
-            Lista a la cual quedaran cargados los elementos despues de la lectura del archivo.
-        sep :: str
-            Separador escodigo para diferenciar a los distintos elementos dentro del archivo.
-    Try:
-        Intenta cargar el archivo CSV a la lista que se le pasa por parametro, si encuentra algun error
-        Borra la lista e informa al usuario
-    Returns: None   
-    """
-    del lst[:]
-    print("Cargando archivo ....")
-    t1_start = process_time() #tiempo inicial
-    dialect = csv.excel()
-    dialect.delimiter=sep
-    try:
-        with open(file, encoding="utf-8") as csvfile:
-            spamreader = csv.DictReader(csvfile, dialect=dialect)
-            for row in spamreader: 
-                lst.append(row)
-    except:
-        del lst[:]
-        print("Se presento un error en la carga del archivo")
+
+# 0) Cargar archivos
+
+casting = lt.newList('SINGLE_LINKED', None)
+# casting_file = "Data/MoviesCastingRaw-small.csv" 
+casting_file = "Data/AllMoviesCastingRaw.csv"
+with open(casting_file, encoding="utf-8-sig") as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=';')
+    for row in reader:
+        lt.addFirst(casting, row)
+
+
+details = lt.newList('SINGLE_LINKED', None)
+# details_file = "Data/SmallMoviesDetailsCleaned.csv" 
+details_file = "Data/AllMoviesDetailsCleaned.csv"
+with open(details_file, encoding="utf-8-sig") as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=';')
+    for row in reader:
+        lt.addFirst(details, row)
+
+# 1) Imprimir menu
+
+print("1- Req 1 - Encontrar buenas películas de un director")
+
+#2) Filtrar
+
+casting_por_director = lt.newList('SINGLE_LINKED', None)
+director = input("Ingrese el director\n")
+
+t1_start = process_time()
+
+iter = listiterator.newIterator(casting)
+while listiterator.hasNext(iter):
+    c = listiterator.next(iter)
+    if c["director_name"] == director:
+        lt.addFirst(casting_por_director, c)
+
+
+details_buenos = lt.newList('SINGLE_LINKED', None)
+
+iter = listiterator.newIterator(details)
+while listiterator.hasNext(iter):
+    d = listiterator.next(iter)
+    if float(d["vote_average"]) > 6:
+        lt.addFirst(details_buenos, d)
     
-    t1_stop = process_time() #tiempo final
-    print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
+# 3) Unir las listas
 
-def printMenu():
-    """
-    Imprime el menu de opciones
-    """
-    print("\nBienvenido")
-    print("1- Cargar Datos")
-    print("2- Contar los elementos de la Lista")
-    print("3- Contar elementos filtrados por palabra clave")
-    print("4- Consultar elementos a partir de dos listas")
-    print("0- Salir")
+union_listas = lt.newList('SINGLE_LINKED', None)
 
-def countElementsFilteredByColumn(criteria, column, lst):
-    """
-    Retorna cuantos elementos coinciden con un criterio para una columna dada  
-    Args:
-        criteria:: str
-            Critero sobre el cual se va a contar la cantidad de apariciones
-        column
-            Columna del arreglo sobre la cual se debe realizar el conteo
-        list
-            Lista en la cual se realizará el conteo, debe estar inicializada
-    Return:
-        counter :: int
-            la cantidad de veces ue aparece un elemento con el criterio definido
-    """
-    if len(lst)==0:
-        print("La lista esta vacía")  
-        return 0
-    else:
-        t1_start = process_time() #tiempo inicial
-        counter=0 #Cantidad de repeticiones
-        for element in lst:
-            if criteria.lower() in element[column].lower(): #filtrar por palabra clave 
-                counter+=1
-        t1_stop = process_time() #tiempo final
-        print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
-    return counter
+iter1 = listiterator.newIterator(details_buenos)
+while listiterator.hasNext(iter1):
+    d = listiterator.next(iter1)
 
-def countElementsByCriteria(criteria, column, lst):
-    """
-    Retorna la cantidad de elementos que cumplen con un criterio para una columna dada
-    """
-    return 0
+    iter2 = listiterator.newIterator(casting_por_director)
+    while listiterator.hasNext(iter2):
+        c = listiterator.next(iter2)
 
+        if not "id" in d:
+            print(d)
 
-def main():
-    """
-    Método principal del programa, se encarga de manejar todos los metodos adicionales creados
+        if d["id"] == c["id"]:
+            union_d_con_c = {**d, **c}
+            lt.addFirst(union_listas, union_d_con_c)
+            break
 
-    Instancia una lista vacia en la cual se guardarán los datos cargados desde el archivo
-    Args: None
-    Return: None 
-    """
-    lista = [] #instanciar una lista vacia
-    while True:
-        printMenu() #imprimir el menu de opciones en consola
-        inputs =input('Seleccione una opción para continuar\n') #leer opción ingresada
-        if len(inputs)>0:
-            if int(inputs[0])==1: #opcion 1
-                loadCSVFile("Data/test.csv", lista) #llamar funcion cargar datos
-                print("Datos cargados, "+str(len(lista))+" elementos cargados")
-            elif int(inputs[0])==2: #opcion 2
-                if len(lista)==0: #obtener la longitud de la lista
-                    print("La lista esta vacía")    
-                else: print("La lista tiene "+str(len(lista))+" elementos")
-            elif int(inputs[0])==3: #opcion 3
-                criteria =input('Ingrese el criterio de búsqueda\n')
-                counter=countElementsFilteredByColumn(criteria, "nombre", lista) #filtrar una columna por criterio  
-                print("Coinciden ",counter," elementos con el crtierio: ", criteria  )
-            elif int(inputs[0])==4: #opcion 4
-                criteria =input('Ingrese el criterio de búsqueda\n')
-                counter=countElementsByCriteria(criteria,0,lista)
-                print("Coinciden ",counter," elementos con el crtierio: '", criteria ,"' (en construcción ...)")
-            elif int(inputs[0])==0: #opcion 0, salir
-                sys.exit(0)
+# 4) Calcular numero de peliculas y promedio
+numero_de_peliculas_director = lt.size(union_listas)
+suma_vote_average = 0.0
 
-if __name__ == "__main__":
-    main()
+iter = listiterator.newIterator(union_listas)
+while listiterator.hasNext(iter):
+    u = listiterator.next(iter)
+    suma_vote_average = suma_vote_average + float(u["vote_average"])
+
+promedio_peliculas = suma_vote_average/numero_de_peliculas_director
+
+# 5) Imprimir resultados
+
+print("Peliculas con vote_average >= 6: " + str(numero_de_peliculas_director))
+print("Promedio (Peliculas con vote_average >= 6): " + str(promedio_peliculas))
+
+t1_stop = process_time() #tiempo final
+print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
